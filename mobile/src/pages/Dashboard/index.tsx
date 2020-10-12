515a5919e814ef0a82b77b2379fac48fc36b37c7
Jefferson Santos <yuiti.santos@gmail.com>
2020-10-11 21:33:35 -0300

    Drawer Navigation

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -3,14 +3,20 @@ import { View, Button }  from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
 
+import { 
+  Container,
+} from './styles';
+
 const Dashboard: React.FC = () => {
   const { signOut } = useAuth();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
-  )
+    <Container>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Sair" onPress={signOut} />
+      </View>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import { View, Button }  from 'react-native';
+
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { useAuth } from '../hooks/auth';
 
 import Dashboard from '../pages/Dashboard';
 
-const App = createStackNavigator();
+const DashboardStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const AppRoutes: React.FC = () => (
-  <App.Navigator
-    screenOptions={{
-      headerShown: false,
-      cardStyle: { backgroundColor: '#312e38' }
-    }}
-  >
-    <App.Screen name="Dashboard" component={Dashboard} />
-  </App.Navigator>
+const DashboardStackScreen = ({navigation}) => (
+  <DashboardStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#80F26D',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold'
+    }
+  }}>
+    <DashboardStack.Screen name="Home" component={Dashboard} options={{
+      title: '',
+      headerLeft: () => (
+        <Icon.Button name="ios-menu" size={35}
+          backgroundColor="#80F26D" onPress={() => navigation.openDrawer()}>
+        </Icon.Button>
+      )
+    }} />
+  </DashboardStack.Navigator>
 );
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={DashboardStackScreen} options={{
+        title: 'Home'
+      }}/>
+    </Drawer.Navigator>
+  );
+};
 
 export default AppRoutes;
